702bedbba0014c95586130f6ed7779c367c52c71
Jonathan Thompson <jonathan.thompson@gmail.com>
2014-11-10 03:54:55 -0800

    Update copyright to current year.

--- jeppetto-test-functional/src/main/java/org/iternine/jeppetto/dao/test/accesscontrol/IdentifiedCreatableObject.java
+++ jeppetto-test-functional/src/main/java/org/iternine/jeppetto/dao/test/accesscontrol/IdentifiedCreatableObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Jeppetto and Jonathan Thompson
+ * Copyright (c) 2011-2014 Jeppetto and Jonathan Thompson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
