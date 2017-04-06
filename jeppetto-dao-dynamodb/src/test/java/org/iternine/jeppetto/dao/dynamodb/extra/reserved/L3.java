fb96efbef5c5f5cf68959ebf309b4b72b27ab6a0
Jonathan Thompson <jonathan.thompson@gmail.com>
2017-04-06 13:16:19 -0700

    Update copyright across all files

--- jeppetto-dao-dynamodb/src/test/java/org/iternine/jeppetto/dao/dynamodb/extra/reserved/L3.java
+++ jeppetto-dao-dynamodb/src/test/java/org/iternine/jeppetto/dao/dynamodb/extra/reserved/L3.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2015 Jeppetto and Jonathan Thompson
+ * Copyright (c) 2011-2017 Jeppetto and Jonathan Thompson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
